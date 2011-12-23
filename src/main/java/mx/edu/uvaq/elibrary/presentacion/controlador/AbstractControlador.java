88773eae7fbd4cd8a4d472b3252511835db0502f
Ignacio Arces <arcesino@gmail.com>
2011-12-23 12:20:04 -0600

    #2 Se crea AbstractControlador como clase base de los controladores del sistema.

---
+++ src/main/java/mx/edu/uvaq/elibrary/presentacion/controlador/AbstractControlador.java
@@ -0,0 +1,114 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.uvaq.elibrary.presentacion.controlador;
+
+import java.io.IOException;
+import java.util.Map;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mx.edu.uvaq.elibrary.presentacion.Mensaje;
+
+/**
+ *
+ * @author arcesino
+ */
+public abstract class AbstractControlador extends HttpServlet {
+
+  private HttpServletRequest request;
+  private HttpServletResponse response;
+  private String accion;
+  private Map<String, Mensaje> mensajes;
+  
+  /** 
+   * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  private void processRequest(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    this.request = request;
+    this.response = response;
+    this.accion = getAccion(request);
+    if (accion != null) {
+      ejecutarAccion(this.accion);
+    } else {
+      ejecutarAccionDefecto();
+    }
+  }
+
+  // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+  /** 
+   * Handles the HTTP <code>GET</code> method.
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    processRequest(request, response);
+  }
+
+  /** 
+   * Handles the HTTP <code>POST</code> method.
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    processRequest(request, response);
+  }
+
+  private String getAccion(HttpServletRequest request) {
+    return request.getParameter("accion");
+  }
+
+  protected String getAccion() {
+    return accion;
+  }
+
+  protected HttpServletRequest getRequest() {
+    return request;
+  }
+
+  protected HttpServletResponse getResponse() {
+    return response;
+  }
+  
+  protected void desplegarVista(String vista, Map modelo) {
+    request.setAttribute("modelo", modelo);
+    request.setAttribute("mensajes", mensajes);
+    RequestDispatcher rd = getServletContext().getNamedDispatcher(vista);
+    try {
+      rd.forward(request, response);
+    } catch (Exception ex) {
+      // TODO: Implementar manejo de excepción en el despliegue.
+    }
+  }
+  
+  protected void accionNoEncontrada() {
+    getResponse().setStatus(HttpServletResponse.SC_NOT_FOUND);
+  }
+  
+  protected Map<String, Mensaje> getMensajes() {
+    return mensajes;
+  }
+
+  protected void agregarMensaje(String clave, Mensaje mensaje) {
+    mensajes.put(clave, mensaje);
+  }
+
+  public abstract void ejecutarAccion(String accion);
+  public abstract void ejecutarAccionDefecto();
+}
